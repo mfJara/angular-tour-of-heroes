1d315c48bc9cc158bcd8f434b981afdefb72028a
mfJara <mfjaras@gmail.com>
2020-06-17 13:19:08 -0500

    first commit - fake cars service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,33 @@
+
+import { MessageService } from './message.service';
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    //TODO: send the message _after_ fetching the hero id
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+}
